7710bdf68f83108e334c5218092ef4876ca27f14
Arnold Kiritu <arnoldkells4@gmail.com>
2026-09-15 21:50:30 +0300

    have ensured the project and todo factories are doing their job

--- src/project.js
+++ src/project.js
@@ -1,7 +1,32 @@
+// import { todo } from "./todo";
+
 function project (name) {
+
   return {
     name: name,
     todos: []
   }
 }
+
+const projects = [];
+
+projects.push(project("Default"));
+projects.push(project("The Odin Project"));
+projects.push(project("Personal"));
+
+todo();
+
+const myTodo = todo(
+  "Finish Todo List",
+  "Complete the Odin Project Todo List",
+  "2026-09-20",
+  "High"
+);
+
+
+projects[1].todos.push(myTodo)
+
+console.log(projects[1].todos);
+
+
 
